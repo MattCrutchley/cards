65a74d0d61047a504b42f044d6c5fd66a9367268
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-13 15:43:59 +0000

    Update draw1.py

--- application/service2/draw1.py
+++ application/service2/draw1.py
@@ -1,23 +1,5 @@
 import random
-
-draw = 0
-while draw <1:
-    randcard = random.randint(1,52)
-    if str(deck.query.filter(deck.id == randcard).all()) != '[]'
-        card1 = deck.query.filter(deck.id == randcard).first()
-        draw += 1
-        return card1
-
-db.session.delete(card1)
-db.session.commit
-
-while draw <2:
-    randcard = random.randint(1,52)
-    if str(deck.query.filter(deck.id == randcard).all()) != '[]'
-        card2 = deck.query.filter(deck.id == randcard).first()
-        draw += 1
-        return card2
-db.session.delete(card2)
-db.session.commit
-
-
+def suit():
+    suits = [Hearts,Spades,Clubs,Diamonds]
+    randsuit = suits[random.randint(0,4)]
+    return randsuit
