cb2b2fdf348cdd476eb0e1a7b743d6bc49da3b43
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-15 11:18:51 +0000

    Update draw1.py

--- application/service2/draw1.py
+++ application/service2/draw1.py
@@ -2,3 +2,21 @@ import random
 def suit():
     suits = [Hearts,Spades,Clubs,Diamonds]
     return randsuit suits[random.randint(0,3)]
+
+
+"""
+def reset()
+    if str(deck.query.all() != '[]'):
+        deckdata = deck.query.all
+        db.session.delete(deckdata)
+        db.session.commit()
+    suits = [Hearts,Spades,Clubs,Diamonds]
+    cards={"2","3","4","5","6","7","8","9","10","J","K","Q","A"]
+    for suit in suits:
+    value = 1
+        for card in cards:
+            value += 1
+            carddata = cards(card = card, suit = suit value = value)
+            db.session.add(carddata)
+            db.session.commit()
+"""        
