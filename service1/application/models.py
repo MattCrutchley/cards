b1a84da647e535191a2b7ef001199543ae3ea817
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-16 20:42:53 +0000

    Create models.py

---
+++ service1/application/models.py
@@ -0,0 +1,12 @@
+from application import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+
+class deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card = db.Column(db.String(10))
+    suit = db.Column(db.String(10))
+    value = db.Column(db.Integer)
+
+    def __repr__(self):
+        return ''.join(['id:',str(self.id),'\r\n'
+            'card: ', self.card,'\r\n', 'suit:', str(self.suit), '\r\n','value: ', str(self.value)])
