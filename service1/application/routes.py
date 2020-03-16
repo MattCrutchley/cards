1fdead25810031994ad9c3a0c2a0964a20f49abf
MattCrutchley <mcrutchley8@gmail.com>
2020-03-16 21:58:11 +0000

    draw now working

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,24 +1,33 @@
 import random
-from  sqlalchemy.sql.expression import func
+from  sqlalchemy.sql.expression import func, select
 from application import app, db
 from application.models import deck
+
 
 """
 def card():
     cards=["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
     return cards[random.randint(0,12)]
 
-"""
 
+
+@app.route('/draw')
 def draw():
-    if str(db.query.all()) != '[]':
-        card = deck.order_by(func.rand()).first()
+    if str(deck.query.all()) != '[]':
+        rand = random.randrange(0, db.session.query(deck).count()) 
+        card = db.session.query(deck)[rand].first()
         db.session.delete(card)
-        db.session.commit
-        return "TEST service 1"
-"""
+        db.session.commit()
+        return str(card)
+    return "TEST service 1"
 
-test=["a","b","c","d"]
-#print(test[3])
-print(card())
 """
+@app.route('/draw')
+def draw():
+    if str(deck.query.all()) != '[]':
+        card = db.session.query(deck).order_by(func.rand()).first()
+        db.session.delete(card)
+        db.session.commit()
+        return str(card)
+    return "TEST service 1"
+
