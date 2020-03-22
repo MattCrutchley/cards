04d8175fd196d4b19d2b9f5d26acccae1ce925dc
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-22 17:13:45 +0000

    Update routes.py

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -16,4 +16,4 @@ def reset():
 @app.route('/draw/')
 def draw():
     response = requests.get('http://draw:5000/').json()
-    return render_template('index.html',card = response["Card:"], suit = response ["Suit:"])
+    return render_template('index.html',card0 = response["Card0"], suit0 = response ["Suit0"])
