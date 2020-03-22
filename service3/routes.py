cfe465a97907b048b5dba3e37ba690b9ebc23cd5
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-22 17:57:47 +0000

    Create routes.py

---
+++ service3/routes.py
@@ -0,0 +1,10 @@
+import random
+import jsonify
+from application import app
+
+@app.route('/')
+def prize():
+  randprize = random.randint(50,100)
+  prizejson = { "Prize": randprize }
+  return jsonify(prizejson)
+  
