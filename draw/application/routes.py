1556834f82c3e5d4218001415c5fc2aaccd794bb
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-22 17:12:16 +0000

    added for loop to draw multiple cards and add to the json

--- draw/application/routes.py
+++ draw/application/routes.py
@@ -4,19 +4,21 @@ from application import app, db
 from application.models import deck
 from flask import jsonify
 
-def sqltojson(row):
+def sqltojson(row,x):
     y = row.split()
     c = (y[2])
     s = (y[3].split(":")[1])
     v = (y[5])
-    J  = { "Card:": c, "Suit:" : s, "Value:" : v }
+    J  = { "Card{}".format(x): c, "Suit{}".format(x) : s, "Value{}".format(x) : v }
     return J
 
 @app.route('/')
 def draw():
-    if str(deck.query.all()) != '[]':
-        card = db.session.query(deck).order_by(func.rand()).first()
-        db.session.delete(card)
-        db.session.commit()
-        return jsonify(sqltojson(str(card)))
-    return "TEST service 1"
+    for i in range(2):
+        if str(deck.query.all()) != '[]':
+            card = db.session.query(deck).order_by(func.rand()).first()
+            db.session.delete(card)
+            db.session.commit()       
+            handjson = {}
+            handjson.update(sqltojson(str(card),str(i))          
+    return jsonify(handjson)
