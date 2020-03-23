fa6ee8e35e57992400b1d3dd7e2048247b7ca781
MattCrutchley <mcrutchley8@gmail.com>
2020-03-23 15:10:36 +0000

    added prize service

--- prize/application/routes.py
+++ prize/application/routes.py
@@ -1,9 +1,9 @@
 import random
-import jsonify
+from flask  import jsonify
 from application import app
 
 @app.route('/')
 def prize():
-  randprize = random.randint(50,100)
+  randprize = random.randint(0,50)
   prizejson = { "Prize": randprize }
   return jsonify(prizejson)
