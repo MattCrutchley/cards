d6149abcf736ed6fe2684611f19095e443ae449f
MattCrutchley <mcrutchley8@gmail.com>
2020-03-26 14:59:21 +0000

    added tests

--- frontend/tests/views_test.py
+++ frontend/tests/views_test.py
@@ -4,6 +4,10 @@ from flask import url_for
 from flask_testing import TestCase
 
 class TestViews(TestCase):
-    def TestHome(self):
-	response = self.client.get(url_for('/'))
-	self.assertEqual(response.staus_code, 200)
+    def create_app(self):
+        config_name = 'testing'
+        return app
+
+    def test_views_home(self):
+        response = self.client.get(url_for('index'))
+        self.assertEqual(response.staus_code, 200)
