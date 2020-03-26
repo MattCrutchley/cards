fdd24db7736d5af7a6ae6b64421694169591b4c4
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-26 14:27:44 +0000

    Update views_test.py

--- frontend/tests/views_test.py
+++ frontend/tests/views_test.py
@@ -4,5 +4,6 @@ from flask import url_for
 from flask_testing import TestCase
 
 class TestViews(TestCase):
-	response = self.client.get(url_for('/'))
-	self.assertEqual(response.staus_code, 200)
+	def TestHome(self):
+		response = self.client.get(url_for('/'))
+		self.assertEqual(response.staus_code, 200)
