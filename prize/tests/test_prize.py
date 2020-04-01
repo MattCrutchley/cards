31e0c9b4c23019f0aa6e8409f4dc17d532b0c5bb
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-04-01 15:50:09 +0100

    Update test_prize.py

---
+++ prize/tests/test_prize.py
@@ -0,0 +1,13 @@
+import unittest
+from application import app
+from flask import url_for
+from flask_testing import TestCase
+
+class TestViews(TestCase):
+    def create_app(self):
+        config_name = 'testing'
+        return app
+
+    def test_views_home(self):
+        response = self.client.get(url_for('prize'))
+        self.assert(response<100)
