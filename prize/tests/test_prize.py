c42b828a2dad6b142c8507fab84f98d8336b18c9
MattCrutchley <mcrutchley8@gmail.com>
2020-04-02 19:13:11 +0000

    removed pycache

--- prize/tests/test_prize.py
+++ prize/tests/test_prize.py
@@ -9,18 +9,8 @@ class TestViews(TestCase):
     def create_app(self):
         config_name = 'testing'
         return app
+
     def test_prize(self):
         randnum = routes.prize().json
         print(randnum)
-        assert(randnum["Prize"] <100)
-"""
-
-    def test_views_home(self):
-        response = self.get(url_for('prize'))
-        self.assertTrue(response<100)
-
-    def test_hello(self):                                                       
-        response =  self.client.get(url_for('prize'),                       
-                                    content_type='integer')                        
-        self.assertTrue((response.get_data)<100)
-"""
+        assert(randnum["Prize"] < 51)
