30bb190e3e09e4173bd6bd4b3bb8c22658cfc9dd
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-04-03 11:25:50 +0100

    Update azuresetup.sh

--- scripts/azuresetup.sh
+++ scripts/azuresetup.sh
@@ -2,3 +2,6 @@ az group create -n app -l uksouth
 az vm create -g app -n jenkins --image UbuntuLTS --generate-ssh-keys
 az vm create -g app -n master --image UbuntuLTS --generate-ssh-keys
 az vm create -g app -n worker --image UbuntuLTS --generate-ssh-keys
+az vm open-port --resource-group app --name jenkins --port 8080
+az vm open-port --resource-group app --name master --port 80
+
