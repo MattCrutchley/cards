c3c798fc83cc549948b0f0e6d4f86d66fef99816
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-04-03 10:47:29 +0100

    Create azuresetup.sh

---
+++ scripts/azuresetup.sh
@@ -0,0 +1,4 @@
+az group create -n app -l uksouth 
+az vm create -g app -n jenkins --image UbuntuLTS --generate-ssh-keys
+az vm create -g app -n master --image UbuntuLTS --generate-ssh-keys
+az vm create -g app -n worker --image UbuntuLTS --generate-ssh-keys
